6a05c763c72652555d3a020b08a93aae8119a9f2
Hovsep Hayrapetyan <hoso.hayrapetyan@gmail.com>
2023-02-17 21:35:50 +0400

    fixed cmake

---
+++ build/CMakeFiles/fdf.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/iSCSI/FDF/src/Calculations.c" "CMakeFiles/fdf.dir/src/Calculations.c.o" "gcc" "CMakeFiles/fdf.dir/src/Calculations.c.o.d"
+  "/iSCSI/FDF/src/Drawing.c" "CMakeFiles/fdf.dir/src/Drawing.c.o" "gcc" "CMakeFiles/fdf.dir/src/Drawing.c.o.d"
+  "/iSCSI/FDF/src/Helpers.c" "CMakeFiles/fdf.dir/src/Helpers.c.o" "gcc" "CMakeFiles/fdf.dir/src/Helpers.c.o.d"
+  "/iSCSI/FDF/src/Init.c" "CMakeFiles/fdf.dir/src/Init.c.o" "gcc" "CMakeFiles/fdf.dir/src/Init.c.o.d"
+  "/iSCSI/FDF/src/Rotations.c" "CMakeFiles/fdf.dir/src/Rotations.c.o" "gcc" "CMakeFiles/fdf.dir/src/Rotations.c.o.d"
+  "/iSCSI/FDF/src/bmpWriter.c" "CMakeFiles/fdf.dir/src/bmpWriter.c.o" "gcc" "CMakeFiles/fdf.dir/src/bmpWriter.c.o.d"
+  "/iSCSI/FDF/src/getValues.c" "CMakeFiles/fdf.dir/src/getValues.c.o" "gcc" "CMakeFiles/fdf.dir/src/getValues.c.o.d"
+  "/iSCSI/FDF/src/keyboardEvents.c" "CMakeFiles/fdf.dir/src/keyboardEvents.c.o" "gcc" "CMakeFiles/fdf.dir/src/keyboardEvents.c.o.d"
+  "/iSCSI/FDF/src/main.c" "CMakeFiles/fdf.dir/src/main.c.o" "gcc" "CMakeFiles/fdf.dir/src/main.c.o.d"
+  "/iSCSI/FDF/src/minilibxEvents.c" "CMakeFiles/fdf.dir/src/minilibxEvents.c.o" "gcc" "CMakeFiles/fdf.dir/src/minilibxEvents.c.o.d"
+  "/iSCSI/FDF/src/mouseEvent.c" "CMakeFiles/fdf.dir/src/mouseEvent.c.o" "gcc" "CMakeFiles/fdf.dir/src/mouseEvent.c.o.d"
+  "/iSCSI/FDF/src/readFile.c" "CMakeFiles/fdf.dir/src/readFile.c.o" "gcc" "CMakeFiles/fdf.dir/src/readFile.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
